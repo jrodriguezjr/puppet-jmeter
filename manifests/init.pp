3312c7c7dad163f0f08bba731d0be66264e0e30a
Joe Rodriguez Jr <jrod@puppetlabs.com>
2014-06-11 17:12:19 -0500

    remove unzip.

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,10 +23,6 @@ class jmeter(
     ensure => present,
   }
 
-  package { 'unzip':
-    ensure => present,
-  }
-
   exec { 'download-jmeter':
     command => "wget -P /root http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${jmeter_version}.tgz",
     creates => "/root/apache-jmeter-${jmeter_version}.tgz"
@@ -39,7 +35,7 @@ class jmeter(
     require => Exec['download-jmeter'],
   }
 
-  if $jmeter_plugins_install == True {  
+  if $jmeter_plugins_install == True {
     exec { 'download-jmeter-plugins':
       command => "wget -P /root http://jmeter-plugins.googlecode.com/files/JMeterPlugins-${jmeter_plugins_version}.zip",
       creates => '/root/JMeterPlugins-${jmeter_plugins_version}.zip'
